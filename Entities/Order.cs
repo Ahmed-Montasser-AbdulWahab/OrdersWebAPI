8957a7ed8f2987e575a30958ea5e7ab0a93bf015
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-29 20:33:59 +0200

    Add project files.

---
+++ Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class Order
+    {
+        [Key]
+        public Guid OrderId { get; set; }
+
+        [RegularExpression(@"^ORDER_202[4-9]_[0-9]+$")]
+        
+        public string? OrderNumber { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? CustomerName { get; set; }
+
+
+        [Required]
+        public DateTime? OrderDate { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double? TotalAmount { get; set; }
+    }
+
+
+}
