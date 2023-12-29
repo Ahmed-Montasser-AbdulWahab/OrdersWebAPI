5055d6cb0443f5a27b2afcd8f92d81f9a345d330
Ahmed Montasser <ahmed.sharkawy99@hotmail.com>
2023-12-30 00:38:13 +0200

    Adding Entities

--- Entities/OrderItem.cs
+++ Entities/OrderItem.cs
@@ -11,5 +11,21 @@ namespace Entities
     {
         [Key]
         public Guid OrderItemId { get; set; }
+
+        [Required]
+        public Guid OrderId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? ProductName { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double? UnitPrice { get; set;}
+
+        [Range(0, double.MaxValue)]
+        public double? TotalPrice { get; set; }
     }
 }
